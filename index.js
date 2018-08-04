e459159ca98dc24c02a19ba69d07949e6d495539
Patrick Cheng <pat.rick.cheng@hotmail.com>
2018-08-04 16:40:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,9 +23,13 @@ function dec(n){
 }
 
 function makeInt(n){
+<<<<<<< HEAD
   return parseInt(n,10)
 }
 
 function preserveDecimal(n){
   return parseFloat(n)
+=======
+  return pareInt(n)
+>>>>>>> 49a15fbe88713af20af92377840f2de08d7843b5
 }
